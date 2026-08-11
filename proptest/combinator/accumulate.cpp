ebf700cad446507bfa89ca28c7395b82f1dada8d
Won-Wook Hong <jradoo@gmail.com>
2026-08-11 14:27:17 +0000

    Fix accumulate result type erasure

--- proptest/combinator/accumulate.cpp
+++ proptest/combinator/accumulate.cpp
@@ -1,11 +1,14 @@
 #include "proptest/combinator/accumulate.hpp"
+#include "proptest/combinator/combinatorimpl.hpp"
 
 namespace proptest {
 namespace util {
 
 GeneratorCommon accumulateImpl(Function1<ShrinkableBase> gen1, Function1<Function1<ShrinkableBase>> gen2gen, size_t minSize, size_t maxSize)
 {
-    return gen::interval<uint64_t>(minSize, maxSize).flatMap<int>([gen1, gen2gen](const uint64_t& size) -> Generator<int> {
+    auto intervalGen = gen::interval<uint64_t>(minSize, maxSize);
+
+    auto deriveAccumulate = [gen1, gen2gen](const uint64_t& size) {
         return Function1<ShrinkableBase>([gen1, gen2gen, size](Random& rand) {
             ShrinkableBase shr = gen1.callDirect(rand);
             for (size_t i = 0; i < size; i++) {
@@ -14,6 +17,10 @@ GeneratorCommon accumulateImpl(Function1<ShrinkableBase> gen1, Function1<Functio
             }
             return shr;
         });
+    };
+
+    return deriveImpl(intervalGen, [deriveAccumulate](const Any& sizeAny) -> Function1<ShrinkableBase> {
+        return deriveAccumulate(sizeAny.getRef<uint64_t>());
     });
 }
 
